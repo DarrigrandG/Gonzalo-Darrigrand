080a978072d9c3ddcc1047f1159f5b9dbba97fa3
Gonzalo Darrigrand <gonzalo.darrigrand@gmail.com>
2023-01-24 01:22:06 -0300

    e

---
+++ sociales.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.hero');
+ScrollReveal().reveal('.acercacs', {delay: 500});
+ScrollReveal().reveal('.experiencia', {delay: 500});
+ScrollReveal().reveal('.proyectos-recientes', {delay: 500});
+
