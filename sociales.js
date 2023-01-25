29e3d0b5ccf7c609830278cc4d6a6726b7d31aaf
Gonzalo Darrigrand <gonzalo.darrigrand@gmail.com>
2023-01-25 01:00:59 -0300

    f

--- sociales.js
+++ sociales.js
@@ -5,4 +5,11 @@ ScrollReveal().reveal('.proyectos-recientes', {delay: 500});
 ScrollReveal().reveal('.articulos', {delay: 500});
 ScrollReveal().reveal('.testimonios', {delay: 500});
 ScrollReveal().reveal('.contacto', {delay: 500});
+ScrollReveal().reveal('.pie', {delay: 500});
+
+/*<a href="mailto: gonzalo.darrigrand@gmail.com">
+              <button type="button">
+                Contacto <i class="bi bi-envelope-check"></i>
+              </button> 
+            </a>*/
 
