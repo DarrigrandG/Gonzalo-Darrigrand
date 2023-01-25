d3de818b38639c7573f5b2ef47259fd5174f5e23
Gonzalo Darrigrand <gonzalo.darrigrand@gmail.com>
2023-01-24 22:02:12 -0300

    e

--- sociales.js
+++ sociales.js
@@ -1,5 +1,8 @@
-ScrollReveal().reveal('.hero');
+//ScrollReveal().reveal('.hero');
 ScrollReveal().reveal('.acercacs', {delay: 500});
 ScrollReveal().reveal('.experiencia', {delay: 500});
 ScrollReveal().reveal('.proyectos-recientes', {delay: 500});
+ScrollReveal().reveal('.articulos', {delay: 500});
+ScrollReveal().reveal('.testimonios', {delay: 500});
+ScrollReveal().reveal('.contacto', {delay: 500});
 
